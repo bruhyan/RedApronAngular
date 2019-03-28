14b21598430c4cd579cf24562de2b6e31ebd3921
bruhyan <39240792+bruhyan@users.noreply.github.com>
2019-03-28 23:41:46 +0800

    Single Recipe Main Content

---
+++ RedApron/RedApronAngular/src/app/SingleRecipe/MainContent/main-content/main-content.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDividerModule } from '@angular/material/divider';
+
+@Component({
+  selector: 'app-main-content-recipe',
+  templateUrl: './main-content-recipe.component.html',
+  styleUrls: ['./main-content-recipe.component.css']
+})
+export class MainContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- RedApron/RedApronAngular/src/app/app.module.ts
+++ RedApron/RedApronAngular/src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainContentComponent } from './SingleRecipe/MainContent/main-content/main-content.component';
+import { MainViewComponent } from './SingleRecipe/MainView/main-view/main-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainContentComponent,
+    MainViewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatDividerModule,
+    MatListModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
