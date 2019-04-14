9c49b0e0dfad64860bb2fa7cdbe6c1d924d5730e
Minxuan12 <40386284+Minxuan12@users.noreply.github.com>
2019-04-15 00:45:29 +0800

    cart view (static)

--- RedApron/RedApronAngular/src/app/app-routing.module.ts
+++ RedApron/RedApronAngular/src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { RecipeMainViewComponent } from './SingleRecipe/MainView/recipe-main-vie
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './LandingPage/landing-page/landing-page.component';
 import { BrowseCategoryComponent } from './browse/browse-category/browse-category.component';
+import { CartComponent } from './cart/cart.component';
 
 
 const routes: Routes = [
   {path: '', component : LandingPageComponent},
   {path: 'home', component: LandingPageComponent},
   {path: 'recipeMainView', component : RecipeMainViewComponent},
-  {path: 'browse', component :BrowseCategoryComponent}
+  {path: 'browse', component :BrowseCategoryComponent},
+  {path: 'cart', component :CartComponent}
 ];
 
 @NgModule({
