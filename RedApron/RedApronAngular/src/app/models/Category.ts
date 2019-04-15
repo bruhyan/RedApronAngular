835f29ab0e1b17cb4d0b432f41f893a8514cb993
Matthea Loo <e0201537@u.nus.edu>
2019-04-15 20:52:24 +0800

    models folder

---
+++ RedApron/RedApronAngular/src/app/models/Category.ts
@@ -0,0 +1,14 @@
+// export class Category {
+//     categoryId: number
+//     name : string
+//     price: number
+//     isAvailable: boolean
+//     recipes: Recipe[]
+//     subscriptionPlans: SubscriptionPlan[]    
+   
+//     constructor(categoryId?:number,name : string, price: number ,isAvailable: boolean){
+//         this.categoryId = categoryId;
+//         this.name
+//     }
+    
+}
