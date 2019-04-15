ad18f49c4f8eb4b2d9d0795089febd143ab4fac3
bruhyan <39240792+bruhyan@users.noreply.github.com>
2019-04-15 20:55:01 +0800

    session ts

---
+++ RedApron/RedApronAngular/src/app/user.ts
@@ -0,0 +1,15 @@
+export class User {
+    addressLine1: string;
+    addressLine2: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    phoneNumber: string;
+    postalCode: string;
+    userId : number;
+    password: string;
+
+    constructor(addressLine1?: string, addressLine2?) {
+        this.
+    }
+}
