d5c893177e3ae08448a1e6bdc6f1332c8f913580
Minxuan12 <40386284+Minxuan12@users.noreply.github.com>
2019-04-17 01:35:56 +0800

    done pop up and create review

--- RedApron/RedApronAngular/src/app/review-past-recipes/review-past-recipes.component.ts
+++ RedApron/RedApronAngular/src/app/review-past-recipes/review-past-recipes.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, Input } from '@angular/core';
 import { SubscriptionPlanService } from '../service/subscription-plan.service';
 import { RecipeService } from '../service/recipe.service'
 import { Recipe } from '../models/Recipe';
+import { SubscriptionPlan } from '../models/SubscriptionPlan';
+import { MatDialog, MatDialogRef } from '@angular/material';
+import { ReviewCreationComponent } from '../review-past-recipes/review-creation/review-creation.component';
 
 
 @Component({
@@ -12,13 +15,13 @@ import { Recipe } from '../models/Recipe';
 export class ReviewPastRecipesComponent implements OnInit {
 
   @Input() num: number
+
   recipe;
 
-  constructor(private subscriptionPlanService: SubscriptionPlanService, private recipeService: RecipeService) { }
+  constructor(private subscriptionPlanService: SubscriptionPlanService, private recipeService: RecipeService, public dialog: MatDialog) { }
 
   ngOnInit() {
     this.retrieveRecipe();
-    console.log("SubscriptionPlan: " + this.num)
   }
 
   retrieveRecipe(){
@@ -26,6 +29,10 @@ export class ReviewPastRecipesComponent implements OnInit {
       this.recipe = res.recipe
       console.log("recipe in past recipes: "+this.recipe);
     });
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(ReviewCreationComponent);
   }
 
 }
---
+++ RedApron/RedApronAngular/src/app/service/review.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs'
+import { catchError } from 'rxjs/operators';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Review } from '../models/Review';
+
+const httpOptions = {
+  header: new HttpHeaders({ 'Content-Type': 'application/json'})
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ReviewService {
+
+  baseUrl = '/api/Review';
+
+  constructor(private httpClient: HttpClient) { }
+
+  createReview(review : Review){
+    
+    let createReviewReq = {
+      "review": review
+    };
+
+    return this.httpClient.put<Review>(this.baseUrl, createReviewReq).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    let errorMessage: String = "";
+
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = "An unknown error has occurred: " + error.error.message;
+    }
+    else {
+      errorMessage = "A HTTP error has occurred: " + `HTTP ${error.status}: ${error.error.message}`;
+    }
+
+    console.error(errorMessage);
+
+    return throwError(errorMessage);
+  }
+}
