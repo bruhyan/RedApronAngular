31241ec8d916c4775910e0edddc0eaea2c0f7768
Matthea Loo <e0201537@u.nus.edu>
2019-04-17 17:18:07 +0800

    css edit

--- RedApron/RedApronAngular/src/app/review-past-recipes/review-past-recipes.component.ts
+++ RedApron/RedApronAngular/src/app/review-past-recipes/review-past-recipes.component.ts
@@ -17,6 +17,10 @@ export class ReviewPastRecipesComponent implements OnInit {
   @Input() num: number
 
   recipe;
+  calories
+  time
+  shortDescription
+  sen : string = ""
 
   constructor(private subscriptionPlanService: SubscriptionPlanService, private recipeService: RecipeService, public dialog: MatDialog) { }
 
@@ -27,9 +31,19 @@ export class ReviewPastRecipesComponent implements OnInit {
   retrieveRecipe(){
     this.recipeService.getRecipeByRecipeId(this.num).subscribe(res => {
       this.recipe = res.recipe
+      this.shortDescription = this.recipe.shortDescription.split(" ")
+      this.calories = this.shortDescription[this.shortDescription.length-1]
+      this.time = this.shortDescription[this.shortDescription.length-2]
+      for (var i = 0; i < this.shortDescription.length-2 ; i++ ){
+       this.sen = this.sen.concat(this.shortDescription[i] + " ")
       console.log("recipe in past recipes: "+this.recipe);
-    });
-  }
+    }
+  },
+    error => {
+      console.log("****** browse category recipe retrieval " + error);
+    }
+  ) 
+}
 
   openDialog(): void {
     const dialogRef = this.dialog.open(ReviewCreationComponent);
